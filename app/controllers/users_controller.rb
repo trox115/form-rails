58252e0d981fe74cace7a5cc84c1a5ca58f4900a
trox115 <tofernandes67@gmail.com>
2019-08-07 21:25:27 +0100

    added form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
     def new
-       
+        @user = User.new
     end
     
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+       #@user = User.new(user_params)
         if @user.save
           redirect_to new_user_path
         else
